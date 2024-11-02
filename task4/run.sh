827a24fec10d0b735b00d4de1931c7187995f83f
Venkata Alapati <VAlapati@lineagelogistics.com>
2024-11-02 09:19:49 -0700

    Adding run file

---
+++ task4/run.sh
@@ -0,0 +1,10 @@
+rm licm.log
+cd ../..
+source venv.sh
+cd cs265-tasks/task4
+# cat ../benchmarks/mem/major-elm.bril | bril2json | python aliasing.py --debug | bril2txt > major-elm.bril
+# cat input.bril | bril2json | python aliasing.py --debug | bril2txt > two-sum.bril
+cat setup_input.bril | bril2json | python aliasing.py --debug | bril2txt > major-elm.bril
+cat major-elm.bril | bril2json | brili > major-elm.out
+cat major-elm.bril | bril2json | brili -p > major-elm.prof
+cat setup_input.bril | bril2json | brili -p > baseline_major-elm.prof
